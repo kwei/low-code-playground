8d233d24e2f584556e9747561ef24352ee15905e
KW <a0979597291@gmail.com>
2022-11-06 11:25:27 +0800

    [what] [why] [how]

--- src/component/Canvas/Canvas.jsx
+++ src/component/Canvas/Canvas.jsx
@@ -22,14 +22,19 @@ const Canvas = () => {
         const id = e.dataTransfer.getData("text");
         if (id === "container") {
             const newNode = document.getElementById(id).cloneNode(true);
+            const resizer = document.getElementById("resizer").cloneNode(true);
             if (!elementStructure.current[e.target.id]) elementStructure.current[e.target.id] = [];
             newNode.id = e.target.id + "." + id + "-" + (elementStructure.current[e.target.id].length+1);
             newNode.className = "container-container-copied";
             newNode.innerHTML = "container";
             newNode.style.height = "auto";
-            newNode.style.marginTop = "30px";
+            newNode.draggable = false;
             newNode.onDrop = handleDrop;
             newNode.onDragOver = handleDragOver;
+            resizer.style.display = "block";
+            resizer.onMouseDown = handleResizeStart;
+            resizer.onMouseUp = handleResizeEnd;
+            newNode.appendChild(resizer);
             newNode.addEventListener("contextmenu", (e) => {
                 e.preventDefault();
                 const contextMenu = document.getElementById("context-menu");
@@ -48,9 +53,34 @@ const Canvas = () => {
         e.preventDefault();
     };
 
+    const resize = (e, ev = null) => {
+        const dx = ev.x - e.x;
+        const dy = ev.y - e.y;
+        console.log(dx, dy);
+        const parentNode = document.getElementById(ev.target.parentNode.id);
+        const currentH = parentNode.clientHeight;
+        const currentW = parentNode.clientWidth;
+        console.log(currentH, currentW);
+        parentNode.style.width = currentW + dx + "px";
+        parentNode.style.height = currentH + dy + "px";
+    };
+
+    const handleResizeStart = (ev) => {
+        console.log("Start resize");
+        ev.preventDefault();
+        document.addEventListener("mousemove", (e) => resize(e, ev), false);
+    };
+
+    const handleResizeEnd = (ev) => {
+        console.log("End resize");
+        ev.preventDefault();
+        document.removeEventListener("mousemove", resize, false);
+    };
+
     return(
         <div id={"container-canvas"} className="container-canvas" onDrop={handleDrop} onDragOver={handleDragOver}>
-
+            <div id={"default-container"} className={"default-container"}></div>
+            <div id={"resizer"} className={"resizer"} onMouseDown={handleResizeStart} onMouseUp={handleResizeEnd}></div>
         </div>
     );
 };
